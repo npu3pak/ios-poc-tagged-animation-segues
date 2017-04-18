c547ab2e3a58071ead721483266c318872d9c468
evsafronov <evsafronov.developer@yandex.ru>
2017-04-18 20:27:04 +0300

    Сделал возврат в начало на последнем экране

---
+++ TransformationKit/FirstViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.swift
+//  TransformationKit
+//
+//  Created by Evgeniy Safronov on 18.04.17.
+//  Copyright © 2017 Evgeniy Safronov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        
+    }
+
+}
